0c7256a304de9381c3499642247c20a8a7510b95
saltukkilinc <112329706+saltukkilinc@users.noreply.github.com>
2023-07-25 15:32:55 +0300

    jump the new learning project

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import { Button } from "@mui/material";
-import Navbar from "./components/navbar";
+
+
 
 function App() {
   return (
     <>
-      <Button
-        variant="contained"
-        color="success"
-        onClick={() => console.log("hello world")}
-      >
-        Hello World
-      </Button>
-      <Navbar />
+    
+
     </>
   );
 }
