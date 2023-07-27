18608d96ec5745695c20ec598f1e24d81cca5a00
saltukkilinc <112329706+saltukkilinc@users.noreply.github.com>
2023-07-27 13:40:15 +0300

    add muiGrid

--- src/components/muiLayout/index.tsx
+++ src/components/muiLayout/index.tsx
@@ -1,45 +1,63 @@
 import React from "react";
-import { Box, Stack, Divider } from "@mui/material";
+import { Box, Stack, Divider, Grid } from "@mui/material";
+import styled from "@mui/material";
 
 const MuiLayout = () => {
   return (
-    <Stack
-      sx={{
-        border: "1px solid red",
-      }}
-      spacing={2}
-      direction="row"
-      divider={<Divider orientation="vertical" flexItem />}
-    >
-      <Box
-        sx={{
-          backgroundColor: "primary.main",
-          width: "200px",
-          height: "200px",
-          color: "white",
-          "&:hover": {
-            backgroundColor: "primary.dark",
-          },
-        }}
+    <>
+      <Stack
+        direction="row"
+        spacing={2}
+        divider={<Divider orientation="vertical" flexItem />}
       >
-        Hello I am a Box component. By default component is a div but I can
-        change it with a component prop to specify a component like span.
-      </Box>
-      <Box
-        component="span"
-        width="200px"
-        height="200px"
-        bgcolor="primary.main"
-        color="white"
-        sx={{
-          "&:hover": {
-            backgroundColor: "primary.dark",
-          },
-        }}
-      >
-        This box was composed by system props.
-      </Box>
-    </Stack>
+        <Box
+          sx={{
+            backgroundColor: "primary.main",
+            width: "200px",
+            height: "200px",
+            color: "white",
+            "&:hover": {
+              backgroundColor: "primary.dark",
+            },
+          }}
+        >
+          Hello I am a Box component. By default component is a div but I can
+          change it with a component prop to specify a component like span.
+        </Box>
+        <Box
+          component="span"
+          width="200px"
+          height="200px"
+          bgcolor="primary.main"
+          color="white"
+          sx={{
+            "&:hover": {
+              backgroundColor: "primary.dark",
+            },
+          }}
+        >
+          This box was composed by system props.
+        </Box>
+      </Stack>
+
+      <Grid container spacing={2} my={2}>
+        <Grid item xs={12} sm={6} md={4}>
+          <Box bgcolor="primary.main" color="white" height={"200px"}>
+            İtem 1
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <Box bgcolor="primary.main" color="white" height={"200px"}>
+            İtem 2
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <Box bgcolor="primary.main" color="white" height={"200px"}>
+            İtem 3
+          </Box>
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
